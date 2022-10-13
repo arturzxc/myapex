f24d84d9dbc05c52205963ebc9a0ef2399490224
arturzxc <arturzxc@hotmail.com>
2022-10-13 20:01:18 +0100

    offsets and cleanup

--- Offsets.cpp
+++ Offsets.cpp
@@ -3,13 +3,13 @@ namespace offsets
 {
     // buttons
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long IN_ATTACK = 0x075c6260; // [Buttons] -> in_attack
+    const long IN_ATTACK = 0x075cc2e0; // [Buttons] -> in_attack
     // core
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long REGION = 0x140000000;     // [Mine]          -> Region
     const long LEVEL = 0x13c3e38;        // [Miscellaneous] -> LevelName
-    const long LOCAL_PLAYER = 0x1e49268; // [Miscellaneous] -> LocalPlayer
-    const long ENTITY_LIST = 0x1a98758;  // [Miscellaneous] -> cl_entitylist
+    const long LOCAL_PLAYER = 0x1e4f288; // [Miscellaneous] -> LocalPlayer
+    const long ENTITY_LIST = 0x1a9e778;  // [Miscellaneous] -> cl_entitylist
     // entity
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel]    -> m_localOrigin
@@ -25,11 +25,4 @@ namespace offsets
     const long BLEEDOUT_STATE = 0x26d0;             // [RecvTable.DT_Player]            -> m_bleedoutState
     const long ZOOMING = 0x1c01;                    // [RecvTable.DT_Player]            -> m_bZooming
     const long LAST_VISIBLE_TIME = 0x1a48;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
-    // weapon
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long WEAPON = 0x012ef098;          // [ClientClasses] -> CWeaponX //BASEANIMATING FOR NOWWW
-    const long WEAPON_AMMO_IN_CLIP = 0x1624; // [DataMap.CWeaponX] -> m_ammoInClip
-    const long WEAPON_IS_SEMI_AUTO = 0x1b92; // [Miscellaneous] ->CWeaponX!m_isSemiAuto
-
-    const long m_latestPrimaryWeapons = 0x19ec; //weapon of local player?
 }
