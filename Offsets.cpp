144ef652162ff3ab5ebc0f1676b4f164d1d2ef7c
Artur K <arturzxc@hotmail.com>
2022-11-17 12:33:10 +0000

    offsets

--- Offsets.cpp
+++ Offsets.cpp
@@ -1,15 +1,17 @@
+//season 15 offset update
+// 15/11/22
 #pragma once
 namespace offsets
 {
     // buttons
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long IN_ATTACK = 0x0763f9f0; // [Buttons] -> in_attack
+    const long IN_ATTACK = 0x07640a70; // [Buttons] -> in_attack
     // core
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long REGION = 0x140000000;     // [Mine]          -> Region
-    const long LEVEL = 0x1431a28;        // [Miscellaneous] -> LevelName
-    const long LOCAL_PLAYER = 0x1ebe3e8; // [Miscellaneous] -> LocalPlayer
-    const long ENTITY_LIST = 0x1b0d678;  // [Miscellaneous] -> cl_entitylist
+    const long LEVEL = 0x1432a28;        // [Miscellaneous] -> LevelName
+    const long LOCAL_PLAYER = 0x1ebf488; // [Miscellaneous] -> LocalPlayer
+    const long ENTITY_LIST = 0x1b0e678;  // [Miscellaneous] -> cl_entitylist
     // entity
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel]    -> m_localOrigin
@@ -24,5 +26,5 @@ namespace offsets
     const long VIEW_ANGLE = (0x2584 - 0x14);        // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
     const long BLEEDOUT_STATE = 0x2718;             // [RecvTable.DT_Player]            -> m_bleedoutState
     const long ZOOMING = 0x1c31;                    // [RecvTable.DT_Player]            -> m_bZooming
-    const long LAST_VISIBLE_TIME = 0x1a70;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
+    const long LAST_VISIBLE_TIME = 0x1a70;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime  1st offset from dump
 }
