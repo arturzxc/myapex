27d5dcf7f6c327423ab36833048a95941ab00b11
ximbot <114713345+ximbot@users.noreply.github.com>
2023-01-24 00:59:05 +0530

    Update Offsets.cpp

--- Offsets.cpp
+++ Offsets.cpp
@@ -1,4 +1,4 @@
-//season 15 LTM/Heirloom Event offset update 01/10/23
+//season 15 LTM/Heirloom Event offset update 01/23/23
 //ToggleBot
 
 #pragma once
@@ -6,13 +6,13 @@ namespace offsets
 {
     // buttons
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long IN_ATTACK = 0x076665d8; // [Buttons] -> in_attack
+    const long IN_ATTACK = 0x076687d8; // [Buttons] -> in_attack
     // core
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long REGION = 0x140000000;     // [Mine]          -> Region
-    const long LEVEL = 0x1449F00;        // [Miscellaneous] -> LevelName
-    const long LOCAL_PLAYER = (0x1EDB670 + 0x8); // [Miscellaneous] -> LocalPlayer
-    const long ENTITY_LIST = 0x1b2a578;  // [Miscellaneous] -> cl_entitylist
+    const long LEVEL = 0x144bf10;        // [Miscellaneous] -> LevelName
+    const long LOCAL_PLAYER = (0x01edd7e0 + 0x8); // [Miscellaneous] -> LocalPlayer
+    const long ENTITY_LIST = 0x1b2c6e8;  // [Miscellaneous] -> cl_entitylist
     // entity
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel]    -> m_localOrigin
